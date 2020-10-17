2ac75a575efce33d9113d886d54d112b14283f8b
jerson100 <juamkoo@gmail.com>
2020-10-17 13:20:17 -0500

    Updates

---
+++ precache-manifest.f12a8d20c500fc59345a9c5e6d476de9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919ef9425f6bb214344722cb1f397ef0",
+    "url": "/gastoSemanalReatcJs/index.html"
+  },
+  {
+    "revision": "1c454009285a2308fbe2",
+    "url": "/gastoSemanalReatcJs/static/css/main.677bc2c7.chunk.css"
+  },
+  {
+    "revision": "d3ba6bb181b46207c54f",
+    "url": "/gastoSemanalReatcJs/static/js/2.fe12cda2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gastoSemanalReatcJs/static/js/2.fe12cda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c454009285a2308fbe2",
+    "url": "/gastoSemanalReatcJs/static/js/main.56496416.chunk.js"
+  },
+  {
+    "revision": "c1f2837f32fbe6b61040",
+    "url": "/gastoSemanalReatcJs/static/js/runtime-main.62d3fddc.js"
+  }
+]);
